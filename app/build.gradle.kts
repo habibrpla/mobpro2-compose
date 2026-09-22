76087eaed77cb1acf97644061a5b3b5e38de4b4a
Habib Akbar Aziiz <habibakbara@telkomuniversity.ac.id>
2026-09-23 05:25:29 +0700

    Rename application packages to com.habibakbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 }
 
 android {
-    namespace = "com.indraazimi.mobpro2"
+    namespace = "com.habibakbar.mobpro2"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -22,7 +22,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.indraazimi.mobpro2"
+        applicationId = "com.habibakbar.mobpro2"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
